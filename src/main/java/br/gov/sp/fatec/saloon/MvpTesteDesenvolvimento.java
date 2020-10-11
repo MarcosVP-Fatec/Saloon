21147f76f87bc200c5a1205e6774de665a16e7a8
MarcosVP-Fatec <marcos.pereira47@fatec.sp.gov.br>
2020-10-11 19:30:56 +0000

    Funções de texto diversas

--- src/main/java/br/gov/sp/fatec/saloon/MvpTesteDesenvolvimento.java
+++ src/main/java/br/gov/sp/fatec/saloon/MvpTesteDesenvolvimento.java
@@ -9,22 +9,20 @@ package br.gov.sp.fatec.saloon;
  * 
  */
 
-
+import br.gov.sp.fatec.saloon.model.tool.Texto;
 import br.gov.sp.fatec.saloon.model.tool.Validador;
 
-/**
- * @apiNote - Saloon - Testes de Desenvolvimento
- * @author - MARCOS VINICIO PEREIRA
- * 
- *         Usar a expressão abaixo para executar pelo maven * mvn exec:java
- *         -Dexec.mainClass="br.gov.sp.fatec.saloon.App"
- * 
- */
- 
 public class MvpTesteDesenvolvimento {
 
+    static int LARGURA = 150;
+
     public static void run() {
+        System.out.println(Texto.concatenarChar('A','B','C','D'));
+        System.out.println(Texto.padC("######################################## INÍCIO ########################################", LARGURA, '#'));
+        System.out.println(Texto.strZero("123",10));
+        System.out.println(Texto.strZero(123,10));
         System.out.println( Validador.dvCpf("99999999999") );
+
     }
 
 }
--- src/main/java/br/gov/sp/fatec/saloon/model/tool/Texto.java
+++ src/main/java/br/gov/sp/fatec/saloon/model/tool/Texto.java
@@ -17,6 +17,40 @@ package br.gov.sp.fatec.saloon.model.tool;
         return sTexto == null ? "" : sTexto.substring(0, sTexto.length() < nTam ? sTexto.length() : nTam);
     }
 
+   /**
+     * @apiNote padC( String texto , int nTam, Character cLetra ) -> Retorna o texto no meio do preenchimento de String
+     * @param String -> Texto
+     * @param Int    -> Tamanho que quer que retorne
+     * @param cLetra -> Letra que vai preencher os espaços. Se omitido usará " "
+     * @return String
+     */
+    public static String padC( String texto , int nTam )               { return padC( texto, nTam, ' ');}
+    public static String padC( String texto , int nTam , String letra ){ return padC( texto, nTam, letra.charAt(0)); }
+    public static String padC( String texto , int nTam , Character letra ){ 
+        int nResta = nTam - texto.length();
+        if ( nResta <= 0 ) return texto;
+        int nDireita = nResta / 2;
+        nResta -= nDireita;
+        StringBuffer retorno = new StringBuffer( replicate( letra , nResta ) );
+        retorno.append( texto );
+        retorno.append( replicate( letra, nDireita ) );
+        return retorno.toString();
+    }
+
+   /**
+     * @apiNote padL( String texto , int nTam, Character cLetra ) -> Retorna o texto com preenchimeneto à esquerda do char passado
+     * @param String -> Texto
+     * @param Int    -> Tamanho que quer que retorne
+     * @param cLetra -> Letra que vai preencher os espaços à esquerda. Se omitido usará " "
+     * @return String
+     */
+    public static String padL( String texto , int nTam )               { return padL( texto, nTam, ' ');}
+    public static String padL( String texto , int nTam , String letra ){ return padL( texto, nTam, letra.charAt(0)); }
+    public static String padL( String texto , int nTam , Character letra ){ 
+        if (texto.length() >= nTam ) return texto;
+        return replicate(letra,nTam-texto.length()) + texto;
+    }
+
     /**
      * @apiNote sohDigitos( String numero ) = Verifica se uma String contém somente dígitos
      * @param String numeros
@@ -40,6 +74,45 @@ package br.gov.sp.fatec.saloon.model.tool;
                  } 
         }
         return true;
+    }
+
+    /**
+     * @apiNote concatenaChar( Character... letras ) = Retorna uma String que concatena as char´s passadas
+     * @param Character...
+     * @return String
+     */
+    public static String concatenarChar( Character... letras){
+        StringBuffer retorno = new StringBuffer();
+        for (Character letra : letras) {
+            retorno.append(letra);
+        }
+        return retorno.toString();
+    }
+
+    /**
+     * @apiNote replicate( String texto, int nQtd ) = Retorna uma String replicada n vêzes 
+     * @param String texto
+     * @param nQtd
+     * @return String
+     */
+    public static String replicate( Character cLetra, int nQtd ){ return replicate( cLetra.toString(), nQtd ); }
+    public static String replicate( String cTexto, int nQtd ){
+        StringBuffer retorno = new StringBuffer( cTexto );
+        for (int i = 1; i < nQtd; i++) {
+            retorno.append( cTexto );
+        }
+        return retorno.toString();
+    }
+
+   /**
+     * @apiNote strZero( String texto , int nTam ) -> Retorna a string preenchida com zeros à esquerda
+     * @param String -> Texto
+     * @param Int    -> Tamanho que quer que retorne
+     * @return String
+     */
+     public static String strZero( int numero, int nTam){ return strZero( Integer.toString( numero ), nTam );}
+     public static String strZero( String texto, int nTam){
+        return padL( texto , nTam, '0');
     }
 
 }
