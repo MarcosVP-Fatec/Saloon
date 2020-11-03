1bd21084ef95cd6f883b350f09ddfb8bdb6e9ba4
RaianDamaceno <raian.damaceno@gmail.com>
2020-11-03 15:00:53 +0000

    change Controller values

--- src/main/java/br/gov/sp/fatec/saloon/Controller/UsuarioController.java
+++ src/main/java/br/gov/sp/fatec/saloon/Controller/UsuarioController.java
@@ -18,16 +18,16 @@ public class UsuarioController {
   //curl https://8080-d38bc4f4-fde6-4f7b-baa5-208e0e3636ac.ws-us02.gitpod.io/usuario/add 
   //-d nome=Teste API -d email=someemail@someemailprovider.com -d senha=testeteste
 
-  @PostMapping(path="/add") // Map ONLY POST Requests
-  public @ResponseBody String addNewUser (@RequestParam final String nome, @RequestParam final String email,
-          @RequestParam final String senha) {
-      final Usuario usuario = new Usuario() ;
-      usuario.setApelido(nome);
-      usuario.setEmail(email);
-      usuario.setSenha(senha);
-      usuarioRepositorio.save(usuario);
-      return "Saved";
-  }
+//   @PostMapping(path="/add") // Map ONLY POST Requests
+//   public @ResponseBody String addNewUser (@RequestParam final String nome, @RequestParam final String email,
+//           @RequestParam final String senha) {
+//       final Usuario usuario = new Usuario() ;
+//       usuario.setApelido(nome);
+//       usuario.setEmail(email);
+//       usuario.setSenha(senha);
+//       usuarioRepositorio.save(usuario);
+//       return "Saved";
+//   }
 
 
   @GetMapping(path = "/all")
--- src/main/java/br/gov/sp/fatec/saloon/Interface/UsuarioRepositorio.java
+++ src/main/java/br/gov/sp/fatec/saloon/Interface/UsuarioRepositorio.java
@@ -1,11 +1,11 @@
 package br.gov.sp.fatec.saloon.Interface;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
+
 
 import br.gov.sp.fatec.saloon.model.entity.regi.Usuario;
+public interface UsuarioRepositorio extends CrudRepository<Usuario, Long> {
 
-public interface UsuarioRepositorio {
-    public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-
-    }
 }
