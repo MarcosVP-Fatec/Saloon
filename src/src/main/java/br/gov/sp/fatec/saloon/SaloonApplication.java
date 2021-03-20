86c37168ef8b82be51126e91cb121e6c2effbcaf
MarcosVP <marcos.pereira47@fatec.sp.gov.br>
2021-03-19 22:19:35 -0300

    Configuração Spring-boot

---
+++ src/src/main/java/br/gov/sp/fatec/saloon/SaloonApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.saloon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaloonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaloonApplication.class, args);
+	}
+
+}
