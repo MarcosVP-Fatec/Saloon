5942841404f37256c4dd58d83985a50cd78331f7
MarcosVP-Fatec <marcos.pereira47@fatec.sp.gov.br>
2021-03-21 15:33:55 +0000

    Entidade/Tabela Usuario + Service + Testes

--- src/test/java/br/gov/sp/fatec/saloon/model/service/regi/UsuarioService_Tests.java
+++ src/test/java/br/gov/sp/fatec/saloon/model/service/regi/UsuarioService_Tests.java
@@ -1,6 +1,11 @@
 package br.gov.sp.fatec.saloon.model.service.regi;
 
-import java.sql.Date;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.text.ParseException;
+import java.util.Date;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.gov.sp.fatec.saloon.model.entity.regi.Usuario;
 import br.gov.sp.fatec.saloon.model.repository.regi.UsuarioRepository;
 import br.gov.sp.fatec.saloon.model.tool.Data;
 
@@ -22,37 +28,31 @@ public class UsuarioService_Tests {
     @Autowired
     private UsuarioService usuarioServiceRepo;
 
+    public UsuarioService_Tests() throws ParseException {}
+
     final String APELIDO_1 = "#APELIDO_USUÁRIO_1";
     final String NOME_1 = "#NOME_USUÁRIO_1";
+    final String NOME_2 = "#NOME_USUÁRIO_2";
+    Date dtNascimento = Data.toDate("12/04/1969");
+
 
     @Test
-	void testeUsuarioServiceIncluir() {
-        
-        Date dtNascimento = Data.toDate("12/04/1969");
-
-        usuarioServiceRepo.inc(APELIDO_1, "#teste@teste.com.br", "ps123", NOME_1, dtNascimento, "99999999999", 3);
-
-        assertTrue(usuarioNivelRepo.existsById(999999L));
+    void testeUsuarioServiceIncluir()  {
+        usuarioServiceRepo.inc(APELIDO_1, "#teste@teste.com.br", "ps123", NOME_1, dtNascimento, "99999999999", 3L);
+        assertTrue(usuarioRepo.existsByApelido(APELIDO_1));
     }
 
   	@Test
-	void testeDesenvolvimentoServiceAlterar() {
-        
-        usuarioNivelServiceRepo.inc(999999L
-                                   ,"#TESTE_NIVEL"
-                                   , true
-                                   , false
-                                   , true
-                                   , false);
-
-        usuarioNivelServiceRepo.alt(999999L
-                                   ,"#ALT_TESTE_NIVEL"
-                                   , true
-                                   , false
-                                   , true
-                                   , false);
-
-        assertTrue(usuarioNivelRepo.existsByDescr("#ALT_TESTE_NIVEL"));
+	void testeDesenvolvitesteUsuarioServiceAlterar() {
+        Usuario usu = usuarioServiceRepo.inc(APELIDO_1, "#teste@teste.com.br", "ps123", NOME_1, dtNascimento, "99999999999", 3L);
+        usuarioServiceRepo.alt(usu.getId(), APELIDO_1, "#teste@teste.com.br", "ps123", NOME_2, dtNascimento, "99999999999", 3L);
+        usuarioRepo.flush();
+        assertEquals(NOME_2, usu.getNome());
     }    
-    
+
+    @Test
+    void testeUsuarioServiceExcluir() throws ParseException {
+        usuarioServiceRepo.del(usuarioServiceRepo.inc(APELIDO_1, "#teste@teste.com.br", "ps123", NOME_1, dtNascimento, "99999999999", 3L).getId());
+        assertFalse(usuarioRepo.existsByApelido(APELIDO_1));
+    }
 }
